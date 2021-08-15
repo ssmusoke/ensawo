fbe11c3d0cd7cd1aa2ce28a484830b23dfe43d70
Stephen S. Musoke <ssmusoke@gmail.com>
2021-08-16 00:58:33 +0300

    Add a view rendered when the applicaiton is down

---
+++ resources/views/coming-soon.blade.php
@@ -0,0 +1,18 @@
+<x-guest-layout>
+    <div class="">
+        <div class="max-w-sm p-8 mx-auto w-full bg-white shadow rounded-xl h-full text-center">
+
+                    <img class="w-96"
+                         src="{{ asset('img/ensawo-logo.png') }}"
+                         alt="Ensawo">
+
+                <header class="p-4 space-y-1">
+                    <h2 class="text-xl font-semibold tracking-tight">
+                        We are just about to launch<br/><br/>
+
+                        Please check back in a short while....
+                    </h2>
+                </header>
+        </div>
+    </div>
+</x-guest-layout>
