3d130d4128f52fbb4b1e9ad4c7473ca575e1cd63
Stephen S. Musoke <ssmusoke@gmail.com>
2021-08-15 18:43:09 +0300

    Add a test to enable linking a transaction to a user account

--- tests/Feature/ProcessTransactionMessageActionTest.php
+++ tests/Feature/ProcessTransactionMessageActionTest.php
@@ -1,13 +1,14 @@
 <?php
 
-use Akaunting\Money\Money;
 use App\Actions\ProcessTransactionMessageAction;
 use App\Models\Transaction;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Str;
 
 uses(RefreshDatabase::class);
 
-beforeEach(function(){
+beforeEach(function () {
     $this->processorAction = new ProcessTransactionMessageAction();
 });
 
@@ -20,7 +21,7 @@ it('returns empty for a message which is not matched by a template', function ()
     expect($this->processorAction->execute("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris a diam maecenas sed enim ut sem viverra."))->toBeEmpty();
 });
 
-it('can process mtn mobile money messages', function($message, $results){
+it('can process mobile money messages', function ($message, $results) {
     $transactionData = $this->processorAction->execute($message);
     expect($transactionData)->toMatchArray($results);
 
@@ -28,5 +29,13 @@ it('can process mtn mobile money messages', function($message, $results){
     $transaction = Transaction::create($transactionData);
     // now compare the values from the database to those in expected results
     // some transformation has happened so this cannot be array to array comparison
-    expect($transaction->amount)->toEqual(Money::UGX($results['amount'])->getValue());
+    expect($transaction->amount)->toEqual(money_parse(Str::remove(",", $results['amount']), "UGX")->format());
 })->with('mobile-money-transaction-messages');
+
+it('can link a transaction to user', function () {
+    $transaction = Transaction::factory()->create();
+    $user = User::factory()->create();
+
+    $this->actingAs($user)->get(route('link-transaction-to-user', ['uuid' => $transaction->uuid]));
+    $this->assertDatabaseHas('transactions', ['uuid' => $transaction->uuid, 'created_by' => $user->id]);
+});
