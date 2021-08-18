296e2dffbda0af99235ddc8f10024c80af9bea80
Stephen S. Musoke <ssmusoke@gmail.com>
2021-08-18 21:20:59 +0300

    Add the recipient name field

--- resources/views/livewire/message-to-transaction-component.blade.php
+++ resources/views/livewire/message-to-transaction-component.blade.php
@@ -235,6 +235,16 @@
                             </dd>
                         </div>
                     @endisset
+                    @isset($transaction->recipient_name)
+                        <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                            <dt class="text-sm font-medium text-gray-500">
+                                Recipient Name
+                            </dt>
+                            <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                                {{ $transaction->recipient_name }}
+                            </dd>
+                        </div>
+                    @endisset
                     @isset($transaction->reason)
                         <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                             <dt class="text-sm font-medium text-gray-500">
